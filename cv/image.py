8d28ba85d2628557840f9a1539730dc842942533
j30393 <petersu@gapp.nthu.edu.tw>
2025-06-05 18:18:53 +0800

    Feat: error correction

--- cv/image.py
+++ cv/image.py
@@ -2,14 +2,23 @@ import numpy as np
 
 
 def to_32F(image):
-    if image.max() > 1.0:
+    if image.max() > 1.5: # yuheng # I don't think it's a fix. But there are something that are missing here
         image = image / 255.0
+        
     return np.clip(np.float32(image), 0, 1)
 
 
 def to_8U(image):
     if image.max() <= 1.0:
         image = image * 255.0
-    return np.clip(np.uint8(image), 0, 255)
+    else:
+        image = image.astype(np.float32)
+    
+    range_min = np.min(image)
+    range_max = np.max(image)
+    if range_max > range_min:
+        image = (image - range_min) * (255.0 / (range_max - range_min))
+    return np.clip(np.round(image).astype(np.uint8), 0, 255)
+ 
 
 
